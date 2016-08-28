fa521ce43e8326cfa66500c6be98e9cd97432c85
Shusuke Kakiuchi <kakiuchis@gmail.com>
2016-08-28 10:42:31 +0000

    dive19-4

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,12 +1,13 @@
 require 'pusher'
 
-Pusher.app_id = ENV["PUSHER_APP_ID"]
 
 
 if Rails.env.production?
+  Pusher.app_id = ENV["PUSHER_APP_ID_PRODUCTION"]
   Pusher.key = ENV["PUSHER_KEY_PRODUCTION"]
   Pusher.secret = ENV["PUSHER_SECRET_PRODUCTION"]
 else
+  Pusher.app_id = ENV["PUSHER_APP_ID_DEVELOPMENT"]
   Pusher.key = ENV["PUSHER_KEY_DEVELOPMENT"]
   Pusher.secret = ENV["PUSHER_SECRET_DEVELOPMENT"]
 end
